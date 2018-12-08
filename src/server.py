90eae8c80c4a4c1a4aaef3ab9c963b86e3a4bef1
Szu-Min Chen <benchen@cs.utexas.edu>
2018-12-08 17:20:17 -0600

    runnable server

--- src/server.py
+++ src/server.py
@@ -3,7 +3,10 @@ from simple_model import SER
 import librosa
 import torch as th
 import pandas as pd
+from pyAudioAnalysis import audioFeatureExtraction, audioBasicIO
+
 app = Flask(__name__)
+
 @app.route('/')
 def hello():
     return "This is a server"
@@ -11,19 +14,38 @@ def hello():
 @app.route('/', methods=['POST'])
 def process_request():
     data = request.get_json(force=True)
-    y = pd.read_json(data['mfcc']).as_matrix().squeeze()
-    print(type(y))
-    sr = data['sr']
-    mfcc = librosa.feature.mfcc(y=y, sr=int(sr), n_mfcc=40).T # (S, 40)
-    mfcc = th.FloatTensor(mfcc).unsqueeze(0)
-    # load model
-    model = SER(h_size=200, feat_size=mfcc.size(2), class_num=4, dropout=0.)
-    model.cuda()
-    model.eval()
-    model.load_state_dict(th.load('checkpoint/model.pt'))
-    pred = model(mfcc, [mfcc.size(1)], None, None)
+    feature_type = data['feat_type']
+    assert feature_type in ['raw', 'mfcc', 'all']
+    model_input = data['m_in']
+
+    y = pd.read_json(data['feat']).as_matrix().squeeze()
+    # get the sampled raw signal
+    if feature_type == 'raw':
+        sr = data['sr']
+        if model_input == 'mfcc':
+            feat = librosa.feature.mfcc(y=y, sr=int(sr), n_mfcc=40).T # (S, 40)
+            feat = th.FloatTensor(feat).unsqueeze(0)
+        elif model_input == 'all':
+            all_feats, f_names = audioFeatureExtraction.stFeatureExtraction(y, sr, 2048, 512)
+            feat = th.FloatTensor(all_feats.T).unsqueeze(0)
+             
+    # get the mfcc directly
+    elif feature_type == 'mfcc': 
+        feat = th.FloatTensor(y).view(-1, 40).unsqueeze(0)
+
+    elif feature_type == 'all':
+        feat = th.FloatTensor(y).view(-1, 34).unsqueeze(0)
     
-    return pred
+
+    # load model
+    model = SER(h_size=200, feat_size=feat.size(2), class_num=4, dropout=0.)
+    #model.cuda()
+    model.eval()
+    #model.load_state_dict(th.load('checkpoint/model.pt'))
+    pred = model(feat, [feat.size(1)], None, None)
+    pred = pred.max(dim=1)[1].item()
+    
+    return str(pred)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=101)
